a867ad56923da0f4b8d17c2385b8568c8d201b06
c2838 <c2838@yahoo.com.tw>
2024-04-09 21:28:20 +0800

    build: fs system & jsons

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 const port = 3000
+// 記錄短網址的JSON檔路徑
+const URLPath = './public/jsons/shortenURL'
+// 載入Nodejs的fs system
+const code_fs = require('fs')
 
-app.get("/", (req, res) => {
-  res.send("express app for shortURL")
+// 根路徑
+app.get('/', (req, res) => {
+  res.send('Home page')
 })
+
+// 短網址完成後路徑
+app.get('/shorten', (req, res) => {
+  res.send('This is shorter')
+})
+
+//讀取短網址JSON檔
+code_fs.readFile(URLPath, 'utf8', (err,data) => {
+  if (err) console.error('err')
+  else {
+    let shortenURLs = shortenURLs ? JSON.parse(shortenURLs) : []
+    shortenURLs.push(data)
+    code_fs.writeFile(URLPath, JSON.stringify(shortenURLs), err =>{
+      if (err) console.log('wirtten fail')
+      else console.log('written sucess')
+    })
+  }
+})
+
+
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
